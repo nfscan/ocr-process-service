2057b76908c8875fd58d3027a89e43584ee57025
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-10-08 17:12:32 -0300

    Fix IMWRITE_JPEG_QUALITY ref for the newest OpenCV

--- service/opencv.py
+++ service/opencv.py
@@ -11,12 +11,12 @@ class OpenCVIntegration(object):
         img = cv2.imread(filename_in)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-        params = [cv2.cv.CV_IMWRITE_JPEG_QUALITY, 100]
+        params = [cv2.IMWRITE_JPEG_QUALITY, 100]
         cv2.imwrite(filename_out, th, params)
 
     @staticmethod
     def grayscale(filename_in, filename_out):
         img = cv2.imread(filename_in)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        params = [cv2.cv.CV_IMWRITE_JPEG_QUALITY, 100]
+        params = [cv2.IMWRITE_JPEG_QUALITY, 100]
         cv2.imwrite(filename_out, img, params)
